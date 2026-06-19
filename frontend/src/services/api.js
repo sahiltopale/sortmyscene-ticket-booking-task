dfc6d7b487fe07d7eddeaf411b401652b4d527ac
Sahil Topale <sahil.topale2005@student.sfit.ac.in>
2026-06-19 20:43:25 +0530

    Completed full stack ticket booking assignment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://sortmyscene-ticket-booking-task.onrender.com/api",
 });
 
 export default api;
